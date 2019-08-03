448221033192414f3e1a9928b2d6d5956e77c111
Ralf Ebert <info@ralfebert.de>
2019-08-03 11:50:50 +0200

    Project setup CustomDrawing

---
+++ CustomDrawing/App/CustomDrawingAppDelegate.swift
@@ -0,0 +1,14 @@
+// © 2019 Ralf Ebert — CustomDrawing
+
+import UIKit
+
+@UIApplicationMain
+class CustomDrawingAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
